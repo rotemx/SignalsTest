b6e62662c24914e657414ae34efaf5411a58a358
Rotem Meiri <rotemx@users.noreply.github.com>
2024-10-22 17:20:00 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import {Component, computed, Signal, signal, WritableSignal} from "@angular/core";
+import { RouterOutlet }                                      from '@angular/router';
+import {JsonPipe}            from "@angular/common";
+import {FormsModule}         from "@angular/forms";
+declare let window:any;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor() {
+    window['comp'] = this;
+  }
   title = 'SignalsTest';
+  person:WritableSignal<{ name:string }> = signal({name : "Inon is Wrong"})
+  
+  computedPerson = computed(()=>({
+    val:this.person
+  }))
+  
+  onNameChange(name: string) {
+    console.log('name', name);
+    this.person.update(p => {
+      p.name = name;
+      return p
+    })
+  }
 }
